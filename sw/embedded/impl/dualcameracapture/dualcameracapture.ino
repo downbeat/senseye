4bc70d57e3f369ba2c3296af03a5030dfd9921bc
Russ Bielawski <jbielaws@umich.edu>
2012-11-02 22:12:57 -0400

    workaround for front camera pixel value wrap bug
    added a workaround to dualcameracapture sketch to fix the bug with the
    front camera (camera #9) where very dark pixels were wrapping around
    the 8-bit space and becoming white

--- sw/embedded/impl/dualcameracapture/dualcameracapture.ino
+++ sw/embedded/impl/dualcameracapture/dualcameracapture.ino
@@ -137,7 +137,9 @@ void frameCaptureAndTx()
     }
     for(jj = 0; jj < 112; ++jj)
     {
-      imrowsc2[jj]=255-imrow2[jj]+stonymask2[(112*ii)+jj];
+      // russ: the +20 is because the mask isn't actually quite correct
+      // (sometimes black wraps over into white)
+      imrowsc2[jj]=255-imrow2[jj]+stonymask2[(112*ii)+jj]+20;
       //imrowsc2[jj]=imrow2[jj]-stonymask2[(112*ii)+jj];
       // russ: try this if the masked version is not working
       //imrowsc2[jj]=imrow2[jj]-128;
