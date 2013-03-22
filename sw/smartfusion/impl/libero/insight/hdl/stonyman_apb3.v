4ee80d1ce78a733083009d279bd30e06bd0e1aae
Russ Bielawski <jbielaws@umich.edu>
2013-03-21 20:10:51 -0400

    fixed FIFO logic in stonyman driver APB3 wrapper
    there is also some general cleanup of that code.  This verilog works as
    long as the application takes care to throttle itself.  There is no busy
    flag in the stonyman driver to tell an application not to start a new
    conversion (and attempting to do so will cause bad things to happen).

--- sw/smartfusion/impl/libero/insight/hdl/stonyman_apb3.v
+++ sw/smartfusion/impl/libero/insight/hdl/stonyman_apb3.v
@@ -14,17 +14,18 @@
 //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-`define WIDTH               (8)
+`define WIDTH               8
 
 `define MASK_REG_RANGE      (8'hFF)
-`define OFFSET_REG_CTRL     (8'h0)
-`define OFFSET_REG_FLAGS    (8'h4)
-`define OFFSET_REG_DATA     (8'h8)
+`define OFFSET_REG_STATUS   (8'h0)
+`define OFFSET_REG_DATA     (8'h4)
 
 `define FIFO_RDEN_S_IDLE    (2'd0)
 `define FIFO_RDEN_S_RAISE   (2'd1)
 `define FIFO_RDEN_S_WAIT    (2'd2)
 `define FIFO_RDEN_S_READY   (2'd3)
+
+`define REG_FLAGS_RESERVED  (6'd0)
 
 
 //////////////////////////////////////////////////////////////////////
@@ -34,24 +35,24 @@ module stonyman_apb3 ( PCLK, PRESERN, PSEL, PENABLE, PREADY, PSLVERR, PWRITE, PA
                        FULL, EMPTY, BUSY, RDEN, PIXELIN, START_CAPTURE );
 
 /* APB SIGNALS */
-input PCLK;      // clock
-input PRESERN;   // reset
+input PCLK;               // clock
+input PRESERN;            // reset
 
-input               PSEL;
-input               PENABLE;
-output wire         PREADY;
-output wire         PSLVERR;
-input               PWRITE;
-input [31:0]        PADDR;
-input [7:0]         PWDATA;
-output wire [7:0]   PRDATA;
+input                        PSEL;
+input                        PENABLE;
+output wire                  PREADY;
+output wire                  PSLVERR;
+input                        PWRITE;
+input [31:0]                 PADDR;
+input [(`WIDTH-1):0]         PWDATA;
+output wire [(`WIDTH-1):0]   PRDATA;
 
 /* APPLICATION SPECIFIC SIGNALS */
 input FULL;
 input EMPTY;
 input BUSY;
 output wire RDEN;          // active low
-input [7:0] PIXELIN;
+input [(`WIDTH-1):0] PIXELIN;
 output wire START_CAPTURE; // active low
 
 
@@ -59,6 +60,7 @@ wire bus_write_enable;
 wire bus_read_enable;
 
 wire ioreg_ready;
+wire ioreg_rden;
 
 
 // no errors, always ready
@@ -70,13 +72,14 @@ assign bus_write_enable = (PENABLE &&  PWRITE && PSEL);
 assign bus_read_enable = (!PWRITE && PSEL);
 
 
-assign RDEN = !(bus_read_enable && !EMPTY);
+// don't actually read the Fifo if it's empty
+assign RDEN = !(ioreg_rden && !EMPTY);
 
 
 stonyman_ioreg stonyman_ioreg_0( .clk(PCLK),.rst(PRESERN),.wren(bus_write_enable),
-                                 .rden(bus_read_enable),.addr(PADDR),
-                                 .ready(ioreg_ready),.datain(PWDATA),.dataout(PRDATA),.full(~FULL),
-                                 .empty(~EMPTY),.appDatain(PIXELIN),.startCapture(START_CAPTURE) );
+                                 .rden(bus_read_enable),.addr(PADDR),.ready(ioreg_ready),
+                                 .fifoRden(ioreg_rden),.datain(PWDATA),.dataout(PRDATA),.full(FULL),
+                                 .empty(EMPTY),.appDatain(PIXELIN),.startCapture(START_CAPTURE) );
 
 
 endmodule
@@ -84,14 +87,14 @@ endmodule
 
 //////////////////////////////////////////////////////////////////////
 // stonyman_ioreg
-module stonyman_ioreg(clk, rst, wren, rden, addr, ready, datain, dataout, full, empty, appDatain, startCapture);
+module stonyman_ioreg(clk, rst, wren, rden, addr, ready, fifoRden, datain, dataout, full, empty, appDatain, startCapture);
 input clk;
 input rst;
 input wren;
 input rden;
 input [31:0] addr;
 output reg ready;
-
+output reg fifoRden;  // active high
 input [(`WIDTH-1):0] datain;
 output reg [(`WIDTH-1):0] dataout;
 
@@ -102,41 +105,71 @@ input [(`WIDTH-1):0] appDatain;
 output reg startCapture;
 
 
+reg [1:0] fifoRdenState;
+
+
 always@ (posedge clk)
 begin
    if(0 == rst)
    begin
       dataout <= 8'd0;
+      fifoRden <= 1'b0;
+      fifoRdenState <= `FIFO_RDEN_S_IDLE;
    end
    else
    begin
       if(1'b1 == rden)
       begin
-         if(`OFFSET_REG_FLAGS == (`MASK_REG_RANGE&addr))
+         if(`OFFSET_REG_STATUS == (`MASK_REG_RANGE&addr))
          begin
             // TODO: support BUSY
-            dataout <= {6'd0,empty,full};
-            ready <= 1;
+            dataout <= {`REG_FLAGS_RESERVED,empty,full};
+            ready <= 1'b1;
          end
          else if(`OFFSET_REG_DATA == (`MASK_REG_RANGE&addr))
          begin
-            dataout <= appDatain;
-            ready <= 1;
+            case(fifoRdenState)
+               `FIFO_RDEN_S_IDLE:
+               begin
+                  fifoRden <= 1'b1;
+                  fifoRdenState <= `FIFO_RDEN_S_RAISE;
+               end
+               `FIFO_RDEN_S_RAISE:
+               begin
+                  fifoRden <= 1'b0;
+                  fifoRdenState <= `FIFO_RDEN_S_WAIT;
+               end
+               `FIFO_RDEN_S_WAIT:
+               begin
+                  fifoRdenState <= `FIFO_RDEN_S_READY;
+               end
+               `FIFO_RDEN_S_READY:
+               begin
+                  // flipping the bits for now (doesn't need to stay here)
+                  dataout <= ~appDatain;
+                  ready <= 1'b1;
+                  fifoRdenState <= `FIFO_RDEN_S_IDLE;
+               end
+               default:
+               begin
+                  // badness!
+               end
+            endcase
          end
          else
          begin
-            dataout <= 0;
-            ready <= 1;
+            dataout <= `WIDTH'd0;
+            ready <= 1'b1;
          end
       end
       else if(1'b1 == wren)
       begin
-         if(`OFFSET_REG_CTRL == (`MASK_REG_RANGE&addr))
+         if(`OFFSET_REG_STATUS == (`MASK_REG_RANGE&addr))
          begin
             if(0 != (1&datain[0]))
             begin
                startCapture <= 1'b0;
-               ready <= 1;
+               ready <= 1'b1;
             end
          end
          else
@@ -147,7 +180,7 @@ begin
       else
       begin
          startCapture <= 1'b1;
-         ready <= 0;
+         ready <= 1'b0;
       end
    end
 end
