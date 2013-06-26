9a8f8c184faad2cccc9563517f4020c6e3e03a9e
Russ Bielawski <jbielaws@umich.edu>
2013-06-26 09:44:17 -0400

    removed a bunch of old code from tripledisplay...
    including ALL normalization code.  this will almost certainly become the
    new "displayandsave" binary as it is MUCH simpler than the old binary.
    a new "pipe through" binary can be created to do normalization

--- sw/linux/impl/tripledisplayfromwebsock/tripledisplayfromwebsock.c
+++ sw/linux/impl/tripledisplayfromwebsock/tripledisplayfromwebsock.c
@@ -5,6 +5,7 @@
 // 2012-10-29: created as captureStreamDual.c
 // 2012-11-12: split out front end display and save functionality
 // 2013-06-26: split out from frontdisplayandsave.c (this is getting sloppy)
+// 2013-06-26: removed a bunch of old unused stuff (like normalization completely)
 //**************************************************************************************************
 
 
@@ -50,8 +51,6 @@ static unsigned gFlagUserCliValid;
 static unsigned gFlagUserCliHelp;
 static unsigned gFlagUserCliModeSelected;
 static unsigned gFlagNoWriteVideo;
-static unsigned gFlagRawMode;
-static unsigned gFlagDrawGrid;
 
 
 //**************************************************************************************************
@@ -77,24 +76,23 @@ int main(int argc, char** argv)
 
    unsigned numcams;
 
-   IplImage *frame, *framenorm, *framescaledup;
-   uchar *frameloc, *framenormloc, *framescaleduploc;
+   IplImage *frame, *framescaledup;
+   uchar *frameloc, *framescaleduploc;
    uchar framevalmin, framevalmax;
-   IplImage *frame2, *frame2norm, *frame2scaledup;
-   uchar *frame2loc, *frame2normloc, *frame2scaleduploc;
+   IplImage *frame2, *frame2scaledup;
+   uchar *frame2loc, *frame2scaleduploc;
    uchar frame2valmin, frame2valmax;
-   IplImage *frame3, *frame3norm, *frame3scaledup;
-   uchar *frame3loc, *frame3normloc, *frame3scaleduploc;
+   IplImage *frame3, *frame3scaledup;
+   uchar *frame3loc, *frame3scaleduploc;
    uchar frame3valmin, frame3valmax;
 
    // double wide!
-   IplImage *framedoublenorm, *framedoublescaledup;
-   uchar *framedoublenormloc1, *framedoublenormloc2, *framedoublenormloc3,
-         *framedoublescaleduploc1, *framedoublescaleduploc2, *framedoublescaleduploc3;
+   IplImage *framedouble, *framedoublescaledup;
+   uchar *framedoubleloc1, *framedoubleloc2;
 
    // triple wide!
-   IplImage *frametripnorm, *frametripscaledup;
-   uchar *frametripnormloc1, *frametripnormloc2, *frametripnormloc3,
+   IplImage *frametrip, *frametripscaledup;
+   uchar *frametriploc1, *frametriploc2, *frametriploc3,
          *frametripscaleduploc1, *frametripscaleduploc2, *frametripscaleduploc3;
 
    IplImage *gridoverlay;
@@ -123,8 +121,6 @@ int main(int argc, char** argv)
    gFlagUserCliHelp=0;
    gFlagUserCliModeSelected=0;
    gFlagNoWriteVideo=0;
-   gFlagRawMode=0;
-   gFlagDrawGrid=0;
    if(0 != parseargs(argc,argv))
    {
       printusage(argv[0]);
@@ -194,31 +190,27 @@ int main(int argc, char** argv)
 
    // init our frame
    frame = cvCreateImage(cvSize(FRAME_X_Y,FRAME_X_Y), IPL_DEPTH_8U, 1);
-   framenorm = cvCreateImage(cvSize(FRAME_X_Y,FRAME_X_Y), IPL_DEPTH_8U, 1);
    framescaledup = cvCreateImage(cvSize( FRAME_X_Y*SCALINGVAL,
                                          FRAME_X_Y*SCALINGVAL ), IPL_DEPTH_8U, 1);
    frame2 = cvCreateImage(cvSize(FRAME_X_Y,FRAME_X_Y), IPL_DEPTH_8U, 1);
-   frame2norm = cvCreateImage(cvSize(FRAME_X_Y,FRAME_X_Y), IPL_DEPTH_8U, 1);
    frame2scaledup = cvCreateImage(cvSize( FRAME_X_Y*SCALINGVAL,
                                           FRAME_X_Y*SCALINGVAL ), IPL_DEPTH_8U, 1);
    frame3 = cvCreateImage(cvSize(FRAME_X_Y,FRAME_X_Y), IPL_DEPTH_8U, 1);
-   frame3norm = cvCreateImage(cvSize(FRAME_X_Y,FRAME_X_Y), IPL_DEPTH_8U, 1);
    frame3scaledup = cvCreateImage(cvSize( FRAME_X_Y*SCALINGVAL,
                                           FRAME_X_Y*SCALINGVAL ), IPL_DEPTH_8U, 1);
-   framedoublenorm = cvCreateImage(cvSize(FRAME_X_Y*2,FRAME_X_Y), IPL_DEPTH_8U, 1);
+   framedouble = cvCreateImage(cvSize(FRAME_X_Y*2,FRAME_X_Y), IPL_DEPTH_8U, 1);
    framedoublescaledup = cvCreateImage(cvSize( FRAME_X_Y*SCALINGVAL*2,
                                              FRAME_X_Y*SCALINGVAL ), IPL_DEPTH_8U, 1);
-   frametripnorm = cvCreateImage(cvSize(FRAME_X_Y*3,FRAME_X_Y), IPL_DEPTH_8U, 1);
+   frametrip = cvCreateImage(cvSize(FRAME_X_Y*3,FRAME_X_Y), IPL_DEPTH_8U, 1);
    frametripscaledup = cvCreateImage(cvSize( FRAME_X_Y*SCALINGVAL*3,
                                              FRAME_X_Y*SCALINGVAL ), IPL_DEPTH_8U, 1);
    gridoverlay = cvCreateImage(cvSize( FRAME_X_Y*SCALINGVAL*2,
                                        FRAME_X_Y*SCALINGVAL ), IPL_DEPTH_8U, 3);
    // appease the compiler
-   frame2loc = frame2normloc = frame2scaleduploc = 0;
-   frame3loc = frame3normloc = frame3scaleduploc = 0;
-   framedoublenormloc1 = framedoublenormloc2 = framedoublenormloc3 = 0;
-   framedoublescaleduploc1 = framedoublescaleduploc2 = framedoublescaleduploc3 = 0;
-   frametripnormloc1 = frametripnormloc2 = frametripnormloc3 = 0;
+   frame2loc = frame2scaleduploc = 0;
+   frame3loc = frame3scaleduploc = 0;
+   framedoubleloc1 = framedoubleloc2 = 0;
+   frametriploc1 = frametriploc2 = frametriploc3 = 0;
    frametripscaleduploc1 = frametripscaleduploc2 = frametripscaleduploc3 = 0;
 
 
@@ -279,39 +271,39 @@ int main(int argc, char** argv)
       {
          for(ii=0; ii<FRAME_X_Y; ++ii)
          {
-            frametripnormloc1 = (uchar*)(frametripnorm->imageData + (ii*frametripnorm->widthStep));
-            frametripnormloc2 = (uchar*)( frametripnorm->imageData
-                                          + (ii*frametripnorm->widthStep)
-                                          + (frametripnorm->widthStep/3) );
-            frametripnormloc3 = (uchar*)( frametripnorm->imageData
-                                          + (ii*frametripnorm->widthStep)
-                                          + 2*(frametripnorm->widthStep/3) );
+            frametriploc1 = (uchar*)(frametrip->imageData + (ii*frametrip->widthStep));
+            frametriploc2 = (uchar*)( frametrip->imageData
+                                          + (ii*frametrip->widthStep)
+                                          + (frametrip->widthStep/3) );
+            frametriploc3 = (uchar*)( frametrip->imageData
+                                          + (ii*frametrip->widthStep)
+                                          + 2*(frametrip->widthStep/3) );
             for(jj=0; jj<FRAME_X_Y; ++jj)
             {
-               frametripnormloc1[jj]=(unsigned char)indat[0*FRAME_X_Y*2 + ii*FRAME_X_Y+jj];
-               frametripnormloc2[jj]=(unsigned char)indat[1*FRAME_X_Y*2 + ii*FRAME_X_Y+jj];
-               frametripnormloc3[jj]=(unsigned char)indat[2*FRAME_X_Y*2 + ii*FRAME_X_Y+jj];
+               frametriploc1[jj]=(unsigned char)indat[0*FRAME_X_Y*2 + ii*FRAME_X_Y+jj];
+               frametriploc2[jj]=(unsigned char)indat[1*FRAME_X_Y*2 + ii*FRAME_X_Y+jj];
+               frametriploc3[jj]=(unsigned char)indat[2*FRAME_X_Y*2 + ii*FRAME_X_Y+jj];
             }
          }
          // create scaled up image
-         cvResize(frametripnorm,frametripscaledup,CV_INTER_LINEAR);
+         cvResize(frametrip,frametripscaledup,CV_INTER_LINEAR);
       }
       else if(2==numcams)
       {
          for(ii=0; ii<FRAME_X_Y; ++ii)
          {
-            framedoublenormloc1 = (uchar*)(framedoublenorm->imageData + (ii*framedoublenorm->widthStep));
-            framedoublenormloc2 = (uchar*)( framedoublenorm->imageData
-                                          + (ii*framedoublenorm->widthStep)
-                                          + (framedoublenorm->widthStep/2) );
+            framedoubleloc1 = (uchar*)(framedouble->imageData + (ii*framedouble->widthStep));
+            framedoubleloc2 = (uchar*)( framedouble->imageData
+                                          + (ii*framedouble->widthStep)
+                                          + (framedouble->widthStep/2) );
             for(jj=0; jj<FRAME_X_Y; ++jj)
             {
-               framedoublenormloc1[jj]=(unsigned char)indat[0*FRAME_X_Y*2 + ii*FRAME_X_Y+jj];
-               framedoublenormloc2[jj]=(unsigned char)indat[1*FRAME_X_Y*2 + ii*FRAME_X_Y+jj];
+               framedoubleloc1[jj]=(unsigned char)indat[0*FRAME_X_Y + ii*FRAME_X_Y+jj];
+               framedoubleloc2[jj]=(unsigned char)indat[1*FRAME_X_Y + ii*FRAME_X_Y+jj];
             }
          }
          // create scaled up image
-         cvResize(framedoublenorm,framedoublescaledup,CV_INTER_LINEAR);
+         cvResize(framedouble,framedoublescaledup,CV_INTER_LINEAR);
       }
       else
       {
@@ -319,187 +311,21 @@ int main(int argc, char** argv)
          // not actually implemented!
       }
 
-#if 0
-      // find max and min pixel values for normalization
-      // TODO: should be a function?
-      framevalmin = 255;
-      framevalmax = 0;
-      frame2valmin = 255;
-      frame2valmax = 0;
-      frame3valmin = 255;
-      frame3valmax = 0;
-      for(ii = 0; ii < FRAME_X_Y; ++ii)
+      // display picture on screen
+      if(3==numcams)
       {
-         frameloc = (uchar*)(frame->imageData + (ii*frame->widthStep));
-         if(2 <= numcams)
-         {
-            frame2loc = (uchar*)(frame2->imageData + (ii*frame2->widthStep));
-         }
-         if(3 <= numcams)
-         {
-            frame3loc = (uchar*)(frame3->imageData + (ii*frame3->widthStep));
-         }
-         for(jj = 0; jj < FRAME_X_Y; ++jj)
-         {
-            if(framevalmin > (unsigned char)indat[((numcams*ii)*FRAME_X_Y)+jj])
-            {
-               framevalmin = (unsigned char)indat[((numcams*ii)*FRAME_X_Y)+jj];
-            }
-            if(framevalmax < (unsigned char)indat[((numcams*ii)*FRAME_X_Y)+jj])
-            {
-               framevalmax = (unsigned char)indat[((numcams*ii)*FRAME_X_Y)+jj];
-            }
-            frameloc[jj] = (unsigned char)indat[((numcams*ii)*FRAME_X_Y)+jj];
-            if(2 <= numcams)
-            {
-               if(frame2valmin > (unsigned char)indat[((numcams*ii+1)*FRAME_X_Y)+jj])
-               {
-                  frame2valmin = (unsigned char)indat[((numcams*ii+1)*FRAME_X_Y)+jj];
-               }
-               if(frame2valmax < (unsigned char)indat[((numcams*ii+1)*FRAME_X_Y)+jj])
-               {
-                  frame2valmax = (unsigned char)indat[((numcams*ii+1)*FRAME_X_Y)+jj];
-               }
-               frame2loc[jj] = (unsigned char)indat[((numcams*ii+1)*FRAME_X_Y)+jj];
-            }
-            if(3 <= numcams)
-            {
-               if(frame3valmin > (unsigned char)indat[((numcams*ii+1)*FRAME_X_Y)+jj])
-               {
-                  frame3valmin = (unsigned char)indat[((numcams*ii+1)*FRAME_X_Y)+jj];
-               }
-               if(frame3valmax < (unsigned char)indat[((numcams*ii+1)*FRAME_X_Y)+jj])
-               {
-                  frame3valmax = (unsigned char)indat[((numcams*ii+1)*FRAME_X_Y)+jj];
-               }
-               frame3loc[jj] = (unsigned char)indat[((numcams*ii+1)*FRAME_X_Y)+jj];
-            }
-         }
+         cvShowImage("CamCapTripleWide", frametripscaledup);
+         cvShowImage("CamCapTripleWideSmall", frametrip);
       }
-
-
-      // normalize and scale-up for display on screen
-      // russ: this is pretty much a total hack!
-      if(0 != gFlagRawMode)
+      else if(2==numcams)
       {
-         framevalmin = 0;
-         framevalmax = 255;
-         if(2 <= numcams)
-         {
-            frame2valmin = 0;
-            frame2valmax = 255;
-         }
-         if(2 <= numcams)
-         {
-            frame3valmin = 0;
-            frame3valmax = 255;
-         }
-      }
-      // TODO: should be function(s)?
-      for(ii = 0; ii < FRAME_X_Y; ++ii)
-      {
-         frameloc = (uchar*)(frame->imageData + (ii*frame->widthStep));
-         framenormloc = (uchar*)(framenorm->imageData + (ii*framenorm->widthStep));
-         if(2 == numcams)
-         {
-            frame2loc = (uchar*)(frame2->imageData + (ii*frame2->widthStep));
-            frame2normloc = (uchar*)(frame2norm->imageData + (ii*frame2norm->widthStep));
-            frametripnormloc1 = (uchar*)(frametripnorm->imageData + (ii*frametripnorm->widthStep));
-            frametripnormloc2 = (uchar*)( frametripnorm->imageData
-                                          + (ii*frametripnorm->widthStep)
-                                          + (frametripnorm->widthStep/2) );
-         }
-         for(jj = 0; jj < FRAME_X_Y; ++jj)
-         {
-            framenormloc[jj] = (uchar)((frameloc[jj]-framevalmin)*(255.0/framevalmax));
-            if(2 == numcams)
-            {
-               frame2normloc[jj] = (uchar)((frame2loc[jj]-frame2valmin)*(255.0/frame2valmax));
-               frametripnormloc1[jj] = (uchar)((frameloc[jj]-framevalmin)*(255.0/framevalmax));
-               frametripnormloc2[jj] = (uchar)((frame2loc[jj]-frame2valmin)*(255.0/frame2valmax));
-            }
-
-            for(xx=0; xx<SCALINGVAL; ++xx)
-            {
-               framescaleduploc
-                  = (uchar*)( framescaledup->imageData
-                              + (((ii*SCALINGVAL)+xx)*framescaledup->widthStep) );
-               if(2 == numcams)
-               {
-                  frame2scaleduploc
-                     = (uchar*)( frame2scaledup->imageData
-                                 + (((ii*SCALINGVAL)+xx)*frame2scaledup->widthStep) );
-                  // double wide
-                  frametripscaleduploc1
-                     = (uchar*)( frametripscaledup->imageData
-                                 + (((ii*SCALINGVAL)+xx)*frametripscaledup->widthStep) );
-                  frametripscaleduploc2 
-                     = (uchar*)( frametripscaledup->imageData
-                                 + (((ii*SCALINGVAL)+xx)*frametripscaledup->widthStep)
-                                 + (frametripscaledup->widthStep/2) );
-               }
-               for(yy=0; yy<SCALINGVAL; ++yy)
-               {
-                  framescaleduploc[(jj*SCALINGVAL)+yy]
-                     = (uchar)((frameloc[jj]-framevalmin)*(255.0/framevalmax));
-                  if(2 == numcams)
-                  {
-                     frame2scaleduploc[(jj*SCALINGVAL)+yy]
-                        = (uchar)((frame2loc[jj]-frame2valmin)*(255.0/frame2valmax));
-                     // double wide
-                     frametripscaleduploc1[(jj*SCALINGVAL)+yy]
-                        = (uchar)((frameloc[jj]-framevalmin)*(255.0/framevalmax));
-                     frametripscaleduploc2[(jj*SCALINGVAL)+yy]
-                        = (uchar)((frame2loc[jj]-frame2valmin)*(255.0/frame2valmax));
-                  }
-               }
-            }
-         }
-      }
-
-
-      if(0 != gFlagDrawGrid)
-      {
-         // FIXME only works with 2 cameras!
-         assert(2 == numcams);
-         cvCvtColor(framedualscaledup, gridoverlay, CV_GRAY2RGB);
-         cvLine( gridoverlay,
-                 cvPoint((0+FRAME_X_Y)*SCALINGVAL,(FRAME_X_Y/3)*SCALINGVAL),
-                 cvPoint((FRAME_X_Y+FRAME_X_Y)*SCALINGVAL,(FRAME_X_Y/3)*SCALINGVAL),
-                 CV_RGB(255,0,0),1,8,0 );
-         cvLine( gridoverlay,
-                 cvPoint((0+FRAME_X_Y)*SCALINGVAL,(2*FRAME_X_Y/3)*SCALINGVAL),
-                 cvPoint((FRAME_X_Y+FRAME_X_Y)*SCALINGVAL,(2*FRAME_X_Y/3)*SCALINGVAL),
-                 CV_RGB(255,0,0),1,8,0 );
-         cvLine( gridoverlay,
-                 cvPoint((FRAME_X_Y/3+FRAME_X_Y)*SCALINGVAL,0*SCALINGVAL),
-                 cvPoint((FRAME_X_Y/3+FRAME_X_Y)*SCALINGVAL,FRAME_X_Y*SCALINGVAL),
-                 CV_RGB(255,0,0),1,8,0 );
-         cvLine( gridoverlay,
-                 cvPoint((2*FRAME_X_Y/3+FRAME_X_Y)*SCALINGVAL,0*SCALINGVAL),
-                 cvPoint((2*FRAME_X_Y/3+FRAME_X_Y)*SCALINGVAL,FRAME_X_Y*SCALINGVAL),
-                 CV_RGB(255,0,0),1,8,0 );
-         cvShowImage("CamCapDoubleWide", gridoverlay);
+         cvShowImage("CamCapDoubleWide", framedoublescaledup);
+         cvShowImage("CamCapDoubleWideSmall", framedouble);
       }
       else
-#endif // 0
       {
-         // display picture on screen
-         if(3==numcams)
-         {
-            cvShowImage("CamCapTripleWide", frametripscaledup);
-            cvShowImage("CamCapTripleWideSmall", frametripnorm);
-         }
-         else if(2==numcams)
-         {
-            cvShowImage("CamCapDoubleWide", framedoublescaledup);
-            cvShowImage("CamCapDoubleWideSmall", framedoublenorm);
-         }
-         else
-         {
-            cvShowImage("CamCap", framescaledup);
-            cvShowImage("CamCapSmall", framenorm);
-         }
+         cvShowImage("CamCap", framescaledup);
+         cvShowImage("CamCapSmall", frame);
       }
 
 
@@ -514,15 +340,15 @@ int main(int argc, char** argv)
 
          if(3 == numcams)
          {
-            (void)cvSaveImage(outfilenameframe,frametripnorm,0);
+            (void)cvSaveImage(outfilenameframe,frametrip,0);
          }
          if(2 == numcams)
          {
-            (void)cvSaveImage(outfilenameframe,framedoublenorm,0);
+            (void)cvSaveImage(outfilenameframe,framedouble,0);
          }
          else
          {
-            (void)cvSaveImage(outfilenameframe,framenorm,0);
+            (void)cvSaveImage(outfilenameframe,frame,0);
          }
       }
 
@@ -543,17 +369,14 @@ int main(int argc, char** argv)
 
    // release/destroy OpenCV objects
    cvReleaseImage(&frame);
-   cvReleaseImage(&framenorm);
    cvReleaseImage(&framescaledup);
    cvReleaseImage(&frame2);
-   cvReleaseImage(&frame2norm);
    cvReleaseImage(&frame2scaledup);
    cvReleaseImage(&frame3);
-   cvReleaseImage(&frame3norm);
    cvReleaseImage(&frame3scaledup);
-   cvReleaseImage(&framedoublenorm);
+   cvReleaseImage(&framedouble);
    cvReleaseImage(&framedoublescaledup);
-   cvReleaseImage(&frametripnorm);
+   cvReleaseImage(&frametrip);
    cvReleaseImage(&frametripscaledup);
    if(3 == numcams)
    {
@@ -604,11 +427,9 @@ static void printhelp(char *progname)
    fprintf(stderr,"press ESC to end the program (user must have context of the video window!).\n");
    fprintf(stderr,"\n");
    fprintf(stderr,"quick and dirty argument descriptions:\n");
-   fprintf(stderr,"  -g         draw 3x3 grid lines\n");
    fprintf(stderr,"  -h         show help and exit\n");
    fprintf(stderr,"  -o PATH    save video to PATH.  PATH cannot already exist.  incompatible with -q\n");
    fprintf(stderr,"  -q         quiet mode.  won't write out any video.  incompatible with -o\n");
-   fprintf(stderr,"  -r         raw mode.  won't normalize the picture\n");
 }
 
 //
@@ -621,12 +442,9 @@ static int parseargs(int argc, char **argv)
 
    errno=0;
 
-   while ((cc = getopt(argc, argv, "gho:qr")) != EOF)
+   while ((cc = getopt(argc, argv, "ho:q")) != EOF)
    {
       switch (cc) {
-         case 'g':
-            gFlagDrawGrid = 1;
-            break;
          case 'h':
             gFlagUserCliValid = 1;
             gFlagUserCliHelp = 1;
@@ -664,9 +482,6 @@ static int parseargs(int argc, char **argv)
             gFlagUserCliValid = 1;
             gFlagNoWriteVideo = 1;
             gFlagUserCliModeSelected = 1;
-            break;
-         case 'r':
-            gFlagRawMode = 1;
             break;
          default:
             errno=EINVAL;
