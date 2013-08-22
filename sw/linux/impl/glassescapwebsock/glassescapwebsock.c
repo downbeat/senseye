71538142f07f8639e3338f22a30414f7b66f4533
Russ Bielawski <jbielaws@umich.edu>
2013-08-21 23:07:28 -0400

    updated IP address in glassescapwebsock
    to the new IP address downstairs.  really, this should only be the
    default, requiring an IP to be specified on the command line, but I
    haven't gotten around to doing that yet.

--- sw/linux/impl/glassescapwebsock/glassescapwebsock.c
+++ sw/linux/impl/glassescapwebsock/glassescapwebsock.c
@@ -35,7 +35,7 @@
 #define FRAME_LEN             (FRAME_X_Y*FRAME_X_Y)
 #define KEY_QUIT              ('q')
 
-#define INSIGHT_SERV_ADDR     ("141.212.11.167")
+#define INSIGHT_SERV_ADDR     ("141.212.106.252")
 #define INSIGHT_SERV_PORT     (80)
 
 #define dbgPrintOp(msg,opcode) do { \
