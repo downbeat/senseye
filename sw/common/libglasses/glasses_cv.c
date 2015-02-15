191844801e0ad6e3da7229e49a345572699935fe
Russ Bielawski <russ@bielawski.org>
2015-02-15 10:06:04 -0500

    Deprecated glasses_cv.h/c.
    The core functionality of glasses_cv.h/c was poorly duplicated uses of
    cvCopy.  glasses_proto.h/c now populates images from the byte stream in
    a way that is compatible with the original protocol data.  For these
    reasons, glasses_cv.h/c are deprecated.

--- sw/common/libglasses/glasses_cv.c
+++ sw/common/libglasses/glasses_cv.c
@@ -1,13 +1,28 @@
 //**************************************************************************************************
+// Copyright 2012 University of Michigan
+//
+//
 // glasses_cv.c
 //
-// Russ Bielawski
-// 2012-12-07: created
+// SensEye glasses C library image mainpulation functions.  Deprecated.  Populating image buffers
+// is now part of glasses_proto.h/c.  cvCopy can perform the functionality of both
+// glassesConcatenateImages and glassesSplitImages with far more flexibility.
+//
+// !!! DEPRECATED !!!!
+//
+//
+// AUTHOR        FULL NAME             EMAIL ADDRESS
+// Russ          Russ Bielawski        russ@bielawski.org
+//
+// VERSION   DATE        AUTHOR        DESCRIPTION
+// 1.00 00   2012-12-07  Russ          Created.
+// 1.00.01   2015-02-15  Russ          Added a nicer version header.
+//                                     Deprecated glasses_cv.h/c.
 //**************************************************************************************************
 
 
 //**************************************************************************************************
-// includes
+// Includes
 //
 #include "glasses_cv.h"
 
@@ -15,7 +30,7 @@
 
 
 //**************************************************************************************************
-// function definitions
+// Function definitions
 //
 
 //
@@ -61,7 +76,6 @@ void glassesPopulateImages(unsigned numcams, char buf[], IplImage *frameEye, Ipl
 //
 void glassesConcatenateImages(IplImage *frameLeft, IplImage *frameRight, IplImage *frameDual, unsigned numchannels)
 {
-   // FIXME: frame resolutions are hard coded
    unsigned ii,jj,kk;
    unsigned width,height;
    uchar *frameLeftLoc;
--- sw/common/libglasses/glasses_cv.h
+++ sw/common/libglasses/glasses_cv.h
@@ -1,26 +1,41 @@
 #ifndef GLASSES_CV_H
 #define GLASSES_CV_H
 //**************************************************************************************************
-// glasses.h
+// Copyright 2012 University of Michigan
 //
-// Russ Bielawski
-// 2012-12-05: created
+//
+// glasses_cv.h
+//
+// SensEye glasses C library image mainpulation functions.  Deprecated.  Populating image buffers
+// is now part of glasses_proto.h/c.  cvCopy can perform the functionality of both
+// glassesConcatenateImages and glassesSplitImages with far more flexibility.
+//
+// !!! DEPRECATED !!!!
+//
+//
+// AUTHOR        FULL NAME             EMAIL ADDRESS
+// Russ          Russ Bielawski        russ@bielawski.org
+//
+// VERSION   DATE        AUTHOR        DESCRIPTION
+// 1.00 00   2012-12-07  Russ          Created.
+// 1.00.01   2015-02-15  Russ          Added a nicer version header.
+//                                     Deprecated glasses_cv.h/c.
 //**************************************************************************************************
 
 
 //**************************************************************************************************
-// includes
+// Includes
 //
 #include "cv.h"
 
 
 //**************************************************************************************************
-// defines / constants
+// Defines / constants
 //
 
 
 //**************************************************************************************************
-// function prototypes
+// Function prototypes
 //
 void glassesPopulateImages(unsigned numcams, char buf[], IplImage *frameEye, IplImage *frameScene);
 void glassesConcatenateImages(IplImage *frameLeft, IplImage *frameRight, IplImage *frameDual, unsigned numchannels);
@@ -28,4 +43,3 @@ void glassesSplitImages(IplImage *frameDual, IplImage *frameLeft, IplImage *fram
 
 
 #endif // GLASSES_CV_H
-
